128b1f2dfbd650112c78c24110839cbbd4aa6e27
Farhana Ety <134615014+Ety2x@users.noreply.github.com>
2025-04-29 14:24:32 +0600

    Add controllers, models, migrations, and Blade views

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\BlogController; 
 use App\Http\Controllers\BlogsController; 
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\CommentController;
 Route::get('/', function () {
     return view('welcome');
 });
@@ -17,3 +20,18 @@ Route::get('/blog', [BlogController::class, 'show']);
 Route::get('/blogs',[BlogsController::class,'index'])->name('blogs.index');
 Route::get('/blogs/create',[BlogsController::class,'create'])->name('blogs.create');
 Route::post('/blogs/store',[BlogsController::class,'store'])->name('blogs.store');
+// Homepage - Show all posts
+Route::get('/', [PostController::class, 'index'])->name('posts.index');
+
+// Post Routes (using resource controller)
+Route::resource('posts', PostController::class)->except(['index']); // 'index' is already defined above
+
+// Comment Routes
+// Store a new comment for a specific post
+Route::post('/posts/{post}/comments', [CommentController::class, 'store'])->name('comments.store');
+// Show edit form for a specific comment
+Route::get('/comments/{comment}/edit', [CommentController::class, 'edit'])->name('comments.edit');
+// Update a specific comment
+Route::put('/comments/{comment}', [CommentController::class, 'update'])->name('comments.update');
+// Delete a specific comment
+Route::delete('/comments/{comment}', [CommentController::class, 'destroy'])->name('comments.destroy');
